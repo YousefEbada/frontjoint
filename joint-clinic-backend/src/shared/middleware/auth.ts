d83734e5d038c4124a70284d1e291be6ed866aa0
Ayman Elfeky <aywork73@gmail.com>
2025-12-19 17:35:58 +0200

    update(auth): Add auth middleware and generate both access and refresh tokens

--- joint-clinic-backend/src/shared/middleware/auth.ts
+++ joint-clinic-backend/src/shared/middleware/auth.ts
@@ -1,6 +1,21 @@
 import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 // Stub: parse user from JWT or session. For now anonymous.
-export function authOptional(req: Request, _res: Response, next: NextFunction) {
-  (req as any).user = (req as any).user || undefined;
+export function authOptional(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (token) {
+    const jwtSecret = process.env.JWT_ACCESS_TOKEN_SECRET as string;
+    try {
+      const payload = jwt.verify(token, jwtSecret) as { userId: string };
+      (req as any).user = { id: payload.userId };
+    } catch (err) {
+      console.error("authOptional middleware JWT verification error:", (err as Error).message);
+      return res.status(401).json("Unauthorized: Invalid token");
+    }
+  } else {
+    // (req as any).user = undefined;
+     return res.status(401).json("Invalid token");
+  }
+  // (req as any).user = (req as any).user || undefined;
   next();
 }
