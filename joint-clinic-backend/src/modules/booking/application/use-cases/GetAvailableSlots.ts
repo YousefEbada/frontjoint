73a17116ac251274e64620ef957a1c8381d9c1cd
Ayman Elfeky <aywork73@gmail.com>
2025-12-21 13:29:09 +0200

    fix(booking): fix the use case get available slots

--- joint-clinic-backend/src/modules/booking/application/use-cases/GetAvailableSlots.ts
+++ joint-clinic-backend/src/modules/booking/application/use-cases/GetAvailableSlots.ts
@@ -1,8 +1,21 @@
+import { DoctorRepoPort } from "modules/doctor/application/ports/DoctorRepoPort";
 import { NixpendPort } from "modules/integration/ports/NixpendPorts";
 
 export class GetAvailableSlots {
-  constructor(private repo: NixpendPort) {}
-    async exec(doctorId: string, date: string) {
-        return this.repo.getAvailableSlots(doctorId, 'Joint Clinic');
+  constructor(private repo: NixpendPort, private doctorRepo: DoctorRepoPort) {}
+    async exec(doctorId: string) {
+        try {
+          const doctor = await this.doctorRepo.findById(doctorId)
+          if(!doctor || !doctor.nixpendId) {
+            return {ok: false, error: 'Doctor not found or missing Nixpend ID' };
+          }
+          const slots = await this.repo.getAvailableSlots(doctor.nixpendId, 'Joint Clinic');
+          if(!slots.data || slots.data.length === 0) {
+            return {ok: false, error: 'No slots available from Nixpend' };
+          }
+          return {ok: true, slots};
+        } catch (error) {
+          return {ok: false, error: 'Error fetching slots from Nixpend' };
+        }
     }
 }
