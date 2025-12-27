0676e0cfabc4b5d9cf23b424f4a4c2fffb5947a5
YousefEbad <yousefomar6464@gmail.com>
2025-12-27 16:51:05 +0200

    solve useDocrors hook issue

--- joint-clinic-frontend/src/hooks/useDoctors.ts
+++ joint-clinic-frontend/src/hooks/useDoctors.ts
@@ -1,18 +1,19 @@
 import { getDoctors } from "@/lib/api/doctor.api";
+import { Doctor } from "@/types/doctor";
 import { useEffect, useState } from "react";
 
 export const useDoctors = () => {
     // i should use tanstack query instead but i will see where this will reach
-    const [doctors, setDoctors] = useState([]);
+    const [doctors, setDoctors] = useState<Doctor[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
 
     useEffect(() => {
         getDoctors()
-        .then(setDoctors)
-        .catch(err => setError(err.message))
-        .finally(() => setLoading(false))
+            .then(setDoctors)
+            .catch(err => setError(err.message))
+            .finally(() => setLoading(false))
     }, [])
 
-    return {doctors, loading, error};
+    return { doctors, loading, error };
 }
