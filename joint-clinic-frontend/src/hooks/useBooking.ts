fe1c58ce56016e0edb09edce4b53215b975ecd1f
YousefEbad <yousefomar6464@gmail.com>
2026-01-10 23:41:41 +0200

    dsd

--- joint-clinic-frontend/src/hooks/useBooking.ts
+++ joint-clinic-frontend/src/hooks/useBooking.ts
@@ -11,7 +11,7 @@ import {
     PendingBookingData,
 } from "@/lib/api/booking.api";
 import { CreateBookingPayload } from "@/types/booking";
-
+//yuuyuyyuyu
 export const useAvailableSlots = (
     doctorId: string,
     fromDate?: string,
