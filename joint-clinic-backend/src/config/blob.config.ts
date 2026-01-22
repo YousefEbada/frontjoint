ba250b678f48187359c40d5d9d8d53a256ba0a14
YousefEbad <yousefomar6464@gmail.com>
2026-01-22 12:47:04 +0200

    dssds

--- joint-clinic-backend/src/config/blob.config.ts
+++ joint-clinic-backend/src/config/blob.config.ts
@@ -4,17 +4,19 @@ import {
 } from "@azure/storage-blob";
 import { env } from "./env.js";
 
-const accountName = env.AZURE_STORAGE_ACCOUNT_NAME!;
-const accountKey = env.AZURE_STORAGE_ACCOUNT_KEY!;
 export const containerName = env.AZURE_STORAGE_CONTAINER_NAME!;
 
-export const sharedKeyCredential =
-  new StorageSharedKeyCredential(accountName, accountKey);
+const accountName = env.AZURE_STORAGE_ACCOUNT_NAME!;
+const accountKey = env.AZURE_STORAGE_ACCOUNT_KEY!;
+export const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
 
-export const blobServiceClient =
-  new BlobServiceClient(
-    `https://${accountName}.blob.core.windows.net`,
-    sharedKeyCredential
-  );
+/**
+ * In dev we often use Azurite (`UseDevelopmentStorage=true`) which is NOT reachable via
+ * `https://devstoreaccount1.blob.core.windows.net`. Using `fromConnectionString` ensures
+ * the correct local endpoint is used.
+ */
+export const blobServiceClient = env.AZURE_BLOB_CONN
+  ? BlobServiceClient.fromConnectionString(env.AZURE_BLOB_CONN)
+  : new BlobServiceClient(`https://${accountName}.blob.core.windows.net`, sharedKeyCredential);
 
 export const storageAccountName = accountName;
